a4ee810c6ef15abfe8a79c8d30508223d2484f78
Douglas Vitor França da Silva <douglas.vitor@gmail.com>
2023-07-29 22:04:14 -0300

    46. Fatiamento de strings e a função len

---
+++ CURSO_PYTHON/aula46.py
@@ -0,0 +1,15 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[0:6])
+print(variavel[-7:-3])
+print(variavel[::-1])
+print(variavel[0:8:2])
+print(len(variavel))
