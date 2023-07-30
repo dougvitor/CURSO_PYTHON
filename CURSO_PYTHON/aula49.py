e9a48ee055272de305189ca916171c4d22d2cfe5
Douglas Vitor França da Silva <douglas.vitor@gmail.com>
2023-07-29 22:36:16 -0300

    49. Introdução ao try e exception para capturar erros (exceptions)

---
+++ CURSO_PYTHON/aula49.py
@@ -0,0 +1,22 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+numero = input(
+    'Vou dobrar o número que vc digitar: '
+)
+
+try:
+    print('STR:', numero)
+    numero = float(numero)
+    print('FLOAT:', numero)
+    print(f'O dobro de {numero} é {numero * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
